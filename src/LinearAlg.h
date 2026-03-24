1c53ea2a9115334e92f04f31fe64dbeb847ea64b
nathanielbartondev <nathanielbartondev@gmail.com>
2026-03-24 15:45:50 -0400

    2D Vector class introduced, implementation ongoing

--- src/LinearAlg.h
+++ src/LinearAlg.h
@@ -1,9 +1,16 @@
-// DEV NOTE: LinearAlg.h REQUIRES iostream header. Investigate ways to
-//           include this header in the library itself, rather than stubbing it out in
-//           Scratch.cpp
+#include <iostream>
+#include <cmath>
+//#include <typeinfo>
 #ifndef LINEARALG_H
 #define LINEARALG_H
 
+// CLASS MATRIX: Represents a matrix of numbers upon which various operations can be performed.
+//
+// Matrices can be of any numeric type (conventionally 'double' for mosot graphics applications).
+// 
+// For arithmetic operations, both the calling matrix and the second operand matrix must be of
+// identical types. Also be sure to observe operation-specific rules (e.g. addition requires that
+// matrix dimensions of both operands match).
 template <typename T, int ROWS, int COLS>
 class Matrix
 {
@@ -111,8 +118,20 @@ class Matrix
     {
       _elements[irow][icol] = value;
     }
-    // utilities
     //void printFields(); // debug
+    // Adds the input matrix to the calling matrix.
+    // Matrices must have equivalent dimensions and element type.
+    void add(Matrix* operand)
+    {
+      for(int i = 0; i < _rows; i++)
+      {
+        for(int j = 0; j < _columns; j++)
+          _elements[i][j] += operand->getElement(i, j);
+      }
+    }
+    //void multBy(Matrix operand);
+    //void scale(double scalar);
+
     void printElements()
     {
       for(int i = 0; i < _rows; i++)
@@ -123,9 +142,100 @@ class Matrix
       }
       std::cout << std::endl;
     }
-    //void add(Matrix operand);
-    //void multBy(Matrix operand);
+    //void printObjectInfo();
 };
 
+// Class representing 2D vectors and their common mathemetical operations.
+template<typename T>
+class Vec2D
+{
+  private:
+    T* _elements;
+    const int _dimensions = 2;
+  public:
+    Vec2D(T x, T y)
+    {
+      _elements = (T*)calloc(_dimensions, sizeof(T));
+      _elements[0] = x;
+      _elements[1] = y;
+    }
+    // Returns a deep-copied array to prevent direct access to vector elements
+    T* getElements()
+    {
+      T* outElements = (T*)calloc(_dimensions, sizeof(T));
+      for(int i = 0; i < _dimensions; i++)
+      {
+        outElements[i] = _elements[i];
+      }
+      return outElements;
+    }
+    // Returns element at given index (starting at 0)
+    T getElement(int index)
+    {
+      // DEV NOTE: Using this scheme for error handling is sufficient for now.
+      // This scheme allows printing of custom error messages AND immediate program exit.
+      // This scheme will only be used for fatal errors.
+      if(index >= _dimensions || index < 0)
+      {
+        std::cout << "ERROR: getElement() could not retrieve element from vector. "
+          << "Index out of bounds." 
+          << std::endl;
+        std::exit(1);
+      }
+      return _elements[index];
+    }
+    // Getting vector dimensions in this way enforces reference to same
+    // value in memory across all scopes, and (redundantly) ensures _dimensions is not altered.
+    int getDimensions()
+    {
+      return _dimensions;
+    }
+    // Adds a scalar value to this vector. Scalars will always be added to all elements
+    // of the vector.
+    void addScalar(T scalar)
+    {
+      for(int i = 0; i < _dimensions; i++)
+        _elements[i] += scalar;
+    }
+    // Scales this vector by a scalar value. All elements of the vector will be multiplied
+    // by this scalar.
+    void Scale(T scalar)
+    {
+      for(int i = 0; i < _dimensions; i++)
+        _elements[i] *= scalar;
+    }
+    
+    double getMagnitude()
+    {
+      // Perform Pythagorean calculation to obtain 2D vector's magnitude. All values
+      // are typecast to double to avoid rounding/truncation issues.
+      return std::sqrt(((double)_elements[0]*(double)_elements[0]) + 
+          ((double)_elements[1]*(double)_elements[1]));
+    }
 
+    void add(Vec2D* operand)
+    {
+      for(int i = 0; i < _dimensions; i++)
+        _elements[i] += operand->getElement(i);
+    }
+
+    // Displays elements of this vector to the screen
+    void printElements()
+    {
+      std::cout << "{ ";
+      for(int i = 0; i < _dimensions; i++)
+      {
+        std::cout << _elements[i] << " ";
+      }
+      std::cout << "}" << std::endl;
+    }
+    // DEBUG FUNCTION -- Prints all member values for this vector object.
+    void printObjectInfo()
+    {
+      std::cout << "Vec2D Object Info\n"
+        << "Dimensions: " << _dimensions << "\n"
+        << "Elements:   ";
+      printElements();
+    }
+};
 #endif
