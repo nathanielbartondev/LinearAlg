fe456c09e8919bd41055a4b299c95839dd9cf2d8
nathanielbartondev <nathanielbartondev@gmail.com>
2026-03-24 19:46:55 -0400

    Revised Vec2D to use double type only (no template), added non-destructive math operations

--- src/LinearAlg.h
+++ src/LinearAlg.h
@@ -4,7 +4,7 @@
 #ifndef LINEARALG_H
 #define LINEARALG_H
 
-// CLASS MATRIX: Represents a matrix of numbers upon which various operations can be performed.
+// Class Matrix: Represents a matrix of numbers upon which various operations can be performed.
 //
 // Matrices can be of any numeric type (conventionally 'double' for mosot graphics applications).
 // 
@@ -145,24 +145,34 @@ class Matrix
     //void printObjectInfo();
 };
 
-// Class representing 2D vectors and their common mathemetical operations.
-template<typename T>
+// Class Vec2D: Represents 2D vectors and their common mathemetical operations.
 class Vec2D
 {
   private:
-    T* _elements;
+    double* _elements;
     const int _dimensions = 2;
   public:
-    Vec2D(T x, T y)
+    Vec2D()
     {
-      _elements = (T*)calloc(_dimensions, sizeof(T));
+      _elements = (double*)calloc(_dimensions, sizeof(double));
+      _elements[0] = 0;
+      _elements[1] = 0;
+    }
+    Vec2D(double x, double y)
+    {
+      _elements = (double*)calloc(_dimensions, sizeof(double));
       _elements[0] = x;
       _elements[1] = y;
     }
+    // GETTERS:
+    // These functions retrieve or otherwise calculate certain information about this
+    // vector object.
+
+    // Get all elements of this vector object.
     // Returns a deep-copied array to prevent direct access to vector elements
-    T* getElements()
+    double* getElements()
     {
-      T* outElements = (T*)calloc(_dimensions, sizeof(T));
+      double* outElements = (double*)calloc(_dimensions, sizeof(double));
       for(int i = 0; i < _dimensions; i++)
       {
         outElements[i] = _elements[i];
@@ -170,7 +180,7 @@ class Vec2D
       return outElements;
     }
     // Returns element at given index (starting at 0)
-    T getElement(int index)
+    double getElement(int index)
     {
       // DEV NOTE: Using this scheme for error handling is sufficient for now.
       // This scheme allows printing of custom error messages AND immediate program exit.
@@ -190,34 +200,123 @@ class Vec2D
     {
       return _dimensions;
     }
+    
+    double getMagnitude()
+    {
+      // Perform Pythagorean calculation to obtain 2D vector's magnitude. All values
+      // are typecast to double to avoid rounding/truncation issues.
+      return std::sqrt((_elements[0]*_elements[0]) + 
+          (_elements[1]*_elements[1]));
+    }
+
+    void setElements(Vec2D* ref)
+    {
+      for(int i = 0 ; i < _dimensions; i++)
+        _elements[i] = ref->getElement(i);
+    }
+
+    void setElement(int index, double elem)
+    {
+      if(index >= _dimensions || index < 0)
+      {
+        std::cout << "ERROR: setElement() failed. Provided index was out of bounds."
+          << std::endl;
+        std::exit(1);
+      }
+      _elements[index] = elem;
+    }
+
+    // DESTRUCTIVE OPERATIONS
+    // These functions are destructive to the vector object, as they each operate directly
+    // on member _elements. All destructive functions' names are prepended with "dst_".
+
     // Adds a scalar value to this vector. Scalars will always be added to all elements
     // of the vector.
-    void addScalar(T scalar)
+    void dst_addScalar(double scalar)
     {
       for(int i = 0; i < _dimensions; i++)
         _elements[i] += scalar;
     }
     // Scales this vector by a scalar value. All elements of the vector will be multiplied
     // by this scalar.
-    void Scale(T scalar)
+    void dst_scale(double scalar)
     {
       for(int i = 0; i < _dimensions; i++)
         _elements[i] *= scalar;
     }
-    
-    double getMagnitude()
-    {
-      // Perform Pythagorean calculation to obtain 2D vector's magnitude. All values
-      // are typecast to double to avoid rounding/truncation issues.
-      return std::sqrt(((double)_elements[0]*(double)_elements[0]) + 
-          ((double)_elements[1]*(double)_elements[1]));
-    }
 
-    void add(Vec2D* operand)
+    void dst_add(Vec2D* operand)
     {
       for(int i = 0; i < _dimensions; i++)
         _elements[i] += operand->getElement(i);
     }
+    // Normalizes this vector, setting its magnitude to 1.
+    void dst_normalize()
+    {
+      double magnitude = getMagnitude();
+
+      for(int i = 0; i < _dimensions; i++)
+        _elements[i] = _elements[i] / magnitude;
+    }
+
+    double dotProd(Vec2D* operand)
+    {
+      // Magnitude of this object and operand object, respectively
+      //double thisMag = getMagnitude();
+      //double opMag = operand->getMagnitude();
+      Vec2D thisCopy;
+      Vec2D operandCopy;
+      ndst_normalize(&thisCopy);
+      operand->ndst_normalize(&operandCopy);
+      double result = 0;
+      
+      for(int i = 0; i < _dimensions; i++)
+        result += (thisCopy.getElement(i)*operandCopy.getElement(i));
+
+      return result;
+    }
+
+    // NON-DESTRUCTIVE OPERATIONS:
+    // These functions are identical in purpose to their destructive counterparts, but
+    // instead of operating directly on the _elements member of the vector object, they
+    // operate on and return a copy of the vector object, thus preserving the original
+    // object's data for later use. All non-destructive functions' names are prepended
+    // with "ndst_".
+    
+    // Normalizes a copy of this vector object, provided as input arg "result"
+    void ndst_normalize(Vec2D* result)
+    {
+      result->setElements(this);
+
+      double magnitude = getMagnitude();
+
+      for(int i = 0; i < _dimensions; i++)
+        result->setElement(i, result->getElement(i) / magnitude);
+    }
+
+    void ndst_addScalar(Vec2D* result, double scalar)
+    {
+      result->setElements(this);
+      for(int i = 0; i < _dimensions; i++)
+        result->setElement(i, result->getElement(i) + scalar);
+    }
+    // Scales this vector by a scalar value. All elements of the vector will be multiplied
+    // by this scalar.
+    void ndst_scale(Vec2D* result, double scalar)
+    {
+      result->setElements(this);
+      for(int i = 0; i < _dimensions; i++)
+        result->setElement(i, result->getElement(i) * scalar);
+    }
+
+    void ndst_add(Vec2D* result, Vec2D* operand)
+    {
+      result->setElements(this);
+      for(int i = 0; i < _dimensions; i++)
+        result->setElement(i, result->getElement(i) + operand->getElement(i));
+    }
+    
+    // MISC UTILITY
 
     // Displays elements of this vector to the screen
     void printElements()
@@ -236,6 +335,7 @@ class Vec2D
         << "Dimensions: " << _dimensions << "\n"
         << "Elements:   ";
       printElements();
+      std::cout << "Magnitude: " << getMagnitude() << std::endl;
     }
 };
 #endif
